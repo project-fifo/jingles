392c87706cda3aacd598d5258516cd6a4dc01ba9
Heinz N. Gies <heinz@licenser.net>
2015-03-05 19:49:21 +0100

    aaaaaaahhhhrrrgh

--- app/scripts/controllers/dataset.js
+++ app/scripts/controllers/dataset.js
@@ -11,7 +11,7 @@ angular.module('fifoApp')
     })
 
     $scope.save = function(nets) {
-      wiggle.datasets.put({id: uuid},
+      wiggle.datasets.put({id: uuid, i_fucking_hate_angular: 'networks'},
                           {networks: nets},
                           function success(res) {
                             status.success('Dataset changed')
